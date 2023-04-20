3f5bc1b5264f8dcb8484d6368956ce1f50b7bb0f
Jyotirmaya <93828035+JyotirS420@users.noreply.github.com>
2023-04-21 04:34:38 +0530

    Updates

--- Deadline_6/insert_vdcs.py
+++ Deadline_6/insert_vdcs.py
@@ -1,115 +1,168 @@
-def vdcs_seetry():
-    import mysql.connector
+import mysql.connector
+from check_float import *
 
 
-    # connect to the database
-    try:
-        mydb = mysql.connector.connect(
-            host="localhost",
-            user="root",
-            passwd="farhan",
-            database="All_Levels"
-        )
-    except:
-        print("Connection failed.")
-        print("Exiting...")
-        exit()
+def insert_VDCS(dmu_code,cursor, mydb):
 
-    #print("Connected to the database.")
-    mycursor = mydb.cursor()
 
+    mycursor = cursor
 
     # store all the data from the VDCS table in a list
     mycursor.execute("SELECT * FROM VDCS")
     vdcs_list = mycursor.fetchall()
-    #print(vdcs_list)
+    # print(vdcs_list)
 
     # Extracting the VDCS ID of each element in vdcs_list and storing it in a list
     vdcs_id_list = [i[0] for i in vdcs_list]
-    #print(vdcs_id_list)
-
+    # print(vdcs_id_list)
 
     n = int(input("Enter number of VDCSs to add (between 0 and 999): "))
-    while(n > 999 or n < 0): # setting a limit of 999 rows to be inserted
-        print("Invalid number of VDCSs. Please enter a valid number of Dairy Farmers (between 0 and 999).")
+    while (n > 999 or n < 0):  # setting a limit of 999 rows to be inserted
+        print("Invalid number of VDCSs.")
         n = int(input("Enter number of VDCSs to add: "))
     print()
 
     # create a list of strings to store the values to be inserted into the table
     values = []
 
-
-    if(n==0):
+    if (n == 0):
         print("No data to insert.")
-        mydb.close()
-        print("Exiting...")
         print()
         return
 
     # take input from the user
     for i in range(n):
 
+        print("VDCS", i + 1, ":")
 
-        print("VDCS", i+1, ":")
-        vdcs_id=""
-        district_id=""
-        while(True):
-            while(len(vdcs_id)!=6):
-                vdcs_id = input("Enter VDCS identification id (Enter 6 digit code): ")
-            while(len(district_id)!=6):
-                district_id = input("Enter DMU identification id (Enter 6 digit code): ")
-            if(district_id[3:]==vdcs_id[:3]):
-                break
-            else:
-                print("VDCS id does not match with DMU id. Please enter a different VDCS identification id.")
-        while(vdcs_id in vdcs_id_list):
-            print("VDCS id already exists. Please enter a different VDCS identification id.")
-            while(True):
-                while(len(vdcs_id)!=6):
-                    vdcs_id = input("Enter VDCS identification id (Enter 6 digit code): ")
-                while(len(district_id)!=6):
-                    district_id = input("Enter DMU identification id (Enter 3 digit code): ")
-                if(district_id[3:]==vdcs_id[:3]):
-                    break
-                else:
-                    print("VDCS id does not match with DMU id. Please enter a different VDCS identification id.")
-        vdcs_id_list.append(vdcs_id) # to prevent inserting duplicate values by the user
+        vdcs_id = input("Enter VDCS ID (3 letters): ")
+        vdcs_id = dmu_code[3:] + vdcs_id.upper()
+        while (len(vdcs_id) != 6 or vdcs_id in vdcs_id_list):
+            print("Invalid VDCS ID. Please enter a valid VDCS ID.")
+            vdcs_id = input("Enter VDCS ID (3 letters): ")
+            vdcs_id = dmu_code[3:] + vdcs_id.upper()
 
-        vdcs_name = input("Enter VDCS name: ")
 
         money = float(input("Enter money present: "))
-        while(money > 999999999999.99 or money < 0):
-            print("Invalid money value. Please enter a valid number (between 0 and 999999999999.99).")
+        while (check_float_range(money) == False):
+            print("Invalid money value.")
             money = float(input("Enter money present: "))
 
         cattle = int(input("Enter cattlefeed amount: "))
-        while(cattle > 999999999999 or cattle < 0):
-            print("Invalid cattlefeed value. Please enter a valid number (between 0 and 999999999999).")
-            id_count = int(input("Enter cattlefeed amount: "))
-        
+        while (check_float_range(cattle) == False):
+            print("Invalid cattlefeed value.")
+            cattle = int(input("Enter cattlefeed amount: "))
+
         milk_quantity = int(input("Enter milk quantity: "))
-        while(milk_quantity > 999999999999 or milk_quantity < 0):
+        while (check_float_range(milk_quantity) == False):
             print("Invalid milk quantity. Please enter a valid number (between 0 and 999999999999).")
             milk_quantity = int(input("Enter milk quantity: "))
 
+        vdcs_name = input("Enter VDCS name: ")
+        while (len(vdcs_name) > 20):
+            print("Invalid VDCS name. Please enter a valid VDCS name.")
+            vdcs_name = input("Enter VDCS name: ")
+
+
         # append the values to the list
-        values.append((vdcs_id, district_id, cattle,money,milk_quantity, vdcs_name))
+        values.append((vdcs_id, cattle, money, milk_quantity, vdcs_name))
         print()
 
-
-
-    # insert the data in values into the table
-    sql = "INSERT INTO VDCS VALUES (%s, %s, %s, %s, %s, %s)"
+    sql = "INSERT INTO VDCS VALUES (%s, %s, %s, %s, %s)"
     mycursor.executemany(sql, values)
 
-    # commit the changes to the database
     mydb.commit()
 
-    # print the number of rows affected
     print(mycursor.rowcount, "record(s) inserted.")
     print()
 
 
 
-# close the connection
-    mydb.close()
+
+
+
+
+def modify_VDCS(vdcs_code, cursor, mydb):
+
+    mycursor = cursor
+
+
+    cattlefeed = float(input("Enter cattle-feed (quantity):"))
+    while(check_float_range(cattlefeed) == False):
+        print("Invalid cattle-feed. Please enter a valid cattle-feed.")
+        cattlefeed = float(input("Enter cattle-feed (quantity):"))
+
+    money = float(input("Enter money: "))
+    while(check_float_range(money) == False):
+        print("Invalid money. Please enter a valid money.")
+        money = float(input("Enter money: "))
+
+    milk_quantity = float(input("Enter milk quantity: "))
+    while(check_float_range(milk_quantity) == False):
+        print("Invalid milk quantity. Please enter a valid milk quantity.")
+        milk_quantity = float(input("Enter milk quantity: "))
+
+
+
+    sql = "UPDATE VDCS SET Cattlefeed = %s, Money = %s, Milk_Quantity = %s WHERE VDCS_code = %s"
+    val = (cattlefeed, money, milk_quantity, vdcs_code)
+    mycursor.execute(sql, val)
+
+
+    print(mycursor.rowcount, "record(s) modified.")
+    print()
+
+    mydb.commit()
+
+
+
+
+def display_VDCS(vdcs_code, cursor):
+    mycursor = cursor
+    sql = "SELECT * FROM VDCS WHERE vdcs_code = '{vdcs_code}'".format(vdcs_code=vdcs_code)
+
+    mycursor.execute(sql)
+    vdcs_data = mycursor.fetchall()
+
+    print()
+    print("VDCS chosen: ", vdcs_code, sep="")
+    print("------------------------------------------------------------")
+    print("|VDCS Code|\t|Cattle Feed|\t|Money|\t|Milk Quantity|\t|Village Name|")
+    print("------------------------------------------------------------")
+    print(vdcs_data[0][0], "\t\t", vdcs_data[0][1], "\t\t", vdcs_data[0][2], "\t\t", vdcs_data[0][3], "\t\t", vdcs_data[0][4], sep="")
+
+    print()
+    print()
+
+
+
+def select_VDCS_In_DMU(vdcs_chosen, cursor):
+    mycursor = cursor
+    query = """SELECT * FROM VDCS
+                        NATURAL JOIN VDCS_Works_Under_DMU
+                        WHERE VDCS_Works_Under_DMU.district_code = '{VDCS}'""".format(VDCS = vdcs_chosen)
+
+
+    mycursor.execute(query)
+    vdcs_data = mycursor.fetchall()
+
+
+    print("VDCS available:")
+    print("------------------------------------------------------------")
+    print("|VDCS Code|")
+    print("------------------------------------------------------------")
+    count = 1
+    for i in vdcs_data:
+        print(count, ". ", i[0], sep="")
+        count += 1
+    print(count, ". ", "Back", sep="")
+    dfin = int(input("Enter your choice: "))
+    while (dfin > len(vdcs_data)+1 or dfin < 1):
+        print("Invalid Dairy Farmer. Please enter a valid Dairy Farmer.")
+        dfin = int(input("Enter your choice: "))
+    if(dfin!=len(vdcs_data)+1):
+        vdcs_chosen = vdcs_data[dfin - 1][0]
+    else:
+        vdcs_chosen=None
+
+    return vdcs_chosen
